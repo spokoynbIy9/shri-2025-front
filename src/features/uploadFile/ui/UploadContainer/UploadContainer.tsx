36b77170dc8beba1c3b1b40f00c8c8b6678f6236
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-06-21 23:04:43 +0500

    feat: add dnd

--- src/features/uploadFile/ui/UploadContainer/UploadContainer.tsx
+++ src/features/uploadFile/ui/UploadContainer/UploadContainer.tsx
@@ -4,8 +4,13 @@ import { useUploadStore } from '../../model/store';
 import { getSuitableMessage } from '../../utils/getSuitableMessage';
 import { useAnalyseStore } from '../../../analyseFile/model/store';
 import { UploadButton } from '../UploadButton/UploadButton';
+import { useState } from 'react';
 
 export const UploadContainer = () => {
+  const [isDragActive, setIsDragActive] = useState(false);
+  const setUploaded = useUploadStore((state) => state.setUploaded);
+  const setFile = useUploadStore((state) => state.setFile);
+
   const isFileUploaded = useUploadStore((state) => state.isUploaded);
   const errorUpload = useUploadStore((state) => state.error);
 
@@ -15,12 +20,61 @@ export const UploadContainer = () => {
 
   const hasError = Boolean(errorUpload) || Boolean(errorAnalyse);
 
+  // dnd
+  const onDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragActive(true);
+  };
+
+  const onDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    setIsDragActive(true);
+  };
+
+  const onDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (e.currentTarget.contains(e.relatedTarget as Node)) return;
+    setIsDragActive(false);
+  };
+
+  const onDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsDragActive(false);
+
+    const files = e.dataTransfer.files;
+    if (files.length === 0) return;
+
+    const file = files[0];
+
+    const isCSV =
+      file.type === 'text/csv' || file.name.toLowerCase().endsWith('.csv');
+
+    if (!isCSV) {
+      alert('Пожалуйста, загрузите CSV файл.');
+      return;
+    }
+
+    setFile(file);
+    setUploaded(true);
+  };
+
   return (
     <div
       className={classNames(styles.container, {
         [styles.container__uploaded]: !hasError && isFileUploaded,
         [styles.container__failed]: hasError && isFileUploaded,
+        [styles.container__dragActive]: isDragActive,
       })}
+      onDragEnter={onDragEnter}
+      onDragOver={onDragOver}
+      onDragLeave={onDragLeave}
+      onDrop={onDrop}
     >
       <UploadButton
         hasError={hasError}
