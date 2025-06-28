90404d4c4638a790719709994502fb991327c107
Gusarov Anatoly <gusarov.tolia@mail.ru>
2025-06-29 00:25:25 +0500

    fix: correct update status in case error

--- src/features/generateCSV/model/store.ts
+++ src/features/generateCSV/model/store.ts
@@ -2,60 +2,62 @@ import { create } from 'zustand';
 import { API_BASE_URL } from '../../../shared/config/api';
 
 interface GenerateState {
-  error: string | null;
-  isProcessing: boolean;
-  isFinished: boolean;
+	error: string | null;
+	isProcessing: boolean;
+	isFinished: boolean;
 
-  setError: (err: string | null) => void;
-  setIsProcessing: (flag: boolean) => void;
-  setIsFinished: (flag: boolean) => void;
+	setError: (err: string | null) => void;
+	setIsProcessing: (flag: boolean) => void;
+	setIsFinished: (flag: boolean) => void;
 
-  downloadReport: (
-    size: number,
-    withErrors?: 'on' | 'off',
-    maxSpend?: string
-  ) => Promise<void>;
+	downloadReport: (
+		size: number,
+		withErrors?: 'on' | 'off',
+		maxSpend?: string
+	) => Promise<void>;
 }
 
 export const useGenerateStore = create<GenerateState>((set, get) => ({
-  error: null,
-  isProcessing: false,
-  isFinished: false,
+	error: null,
+	isProcessing: false,
+	isFinished: false,
 
-  setError: (error) => set({ error }),
-  setIsProcessing: (flag) => set({ isProcessing: flag }),
-  setIsFinished: (flag) => set({ isFinished: flag }),
+	setError: (error) => set({ error }),
+	setIsProcessing: (flag) => set({ isProcessing: flag }),
+	setIsFinished: (flag) => set({ isFinished: flag }),
 
-  downloadReport: async (
-    size: number,
-    withErrors: 'on' | 'off' = 'off',
-    maxSpend: string = '1000'
-  ) => {
-    const queryParams = new URLSearchParams({
-      size: size.toString(),
-      withErrors,
-      maxSpend,
-    });
+	downloadReport: async (
+		size: number,
+		withErrors: 'on' | 'off' = 'off',
+		maxSpend: string = '1000'
+	) => {
+		const queryParams = new URLSearchParams({
+			size: size.toString(),
+			withErrors,
+			maxSpend,
+		});
 
-    get().setIsProcessing(true);
-    const response = await fetch(`${API_BASE_URL}/report?${queryParams}`);
+		get().setIsProcessing(true);
+		const response = await fetch(`${API_BASE_URL}/report?${queryParams}`);
 
-    if (!response.ok) {
-      get().setError(`Ошибка при получении отчета`);
-      return;
-    }
+		if (!response.ok) {
+			get().setError(`Ошибка при получении отчета`);
+			get().setIsProcessing(false);
 
-    const blob = await response.blob();
-    const url = URL.createObjectURL(blob);
+			return;
+		}
 
-    get().setIsProcessing(false);
-    get().setIsFinished(true);
+		const blob = await response.blob();
+		const url = URL.createObjectURL(blob);
 
-    const anchor = document.createElement('a');
-    anchor.href = url;
-    anchor.download = `report_${Date.now()}.csv`;
-    anchor.click();
+		get().setIsProcessing(false);
+		get().setIsFinished(true);
 
-    URL.revokeObjectURL(url);
-  },
+		const anchor = document.createElement('a');
+		anchor.href = url;
+		anchor.download = `report_${Date.now()}.csv`;
+		anchor.click();
+
+		URL.revokeObjectURL(url);
+	},
 }));
